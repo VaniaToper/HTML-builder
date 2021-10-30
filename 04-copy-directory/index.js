6c4e1240ec0b7d5ec463d52b04b170c32d0de4b8
VaniaToper <vanjaqwe58@gmail.com>
2021-10-30 12:42:20 +0200

    feat🔨: completed fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const path = require('path');
+const fs = require('fs');
+const filePath = path.join(__dirname, 'files-copy');
 
+const dir = path.join(__dirname, 'files');
+function copyDir() {
+  fs.mkdir(filePath, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+  fs.readdir(dir, (err, files) => {
+    if (err) throw err;
+    else {
+      files.forEach((file) => {
+        const baseFile = path.join(__dirname, 'files', file);
+        const copyFile = path.join(__dirname, 'files-copy', file);
+        fs.copyFile(baseFile, copyFile, (err) => {
+          if (err) {
+            throw err;
+          }
+        });
+      });
+    }
+  });
+}
+copyDir();
